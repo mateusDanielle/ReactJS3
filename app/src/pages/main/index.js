bebe62bee46ca71555a569260f2c3086a7b78579
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-07 16:50:47 -0200

    Configuração do Redux

--- app/src/pages/main/index.js
+++ app/src/pages/main/index.js
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
-export default Main;
+import * as FavoriteActions from '../../store/actions/favorites';
+
+class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ).isRequired,
+  };
+
+  state = {
+    repositoryInput: '',
+  };
+
+  handleAddRepository = (e) => {
+    e.preventDefault();
+
+    const { addFavorite } = this.props;
+
+    addFavorite();
+  };
+
+  render() {
+    const { repositoryInput } = this.state;
+    const { favorites } = this.props;
+
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          {favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name}</strong>
+(
+                {favorite.description}
+)
+              </p>
+              <a href={favorite.url}>Acessar</a>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
