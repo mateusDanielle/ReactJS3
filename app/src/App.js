4d77d5cb1eb7c1567859b10ca4546eea4494bba5
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-07 14:31:41 -0200

    Lista de todos

--- app/src/App.js
+++ app/src/App.js
@@ -3,15 +3,12 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 
-// class App extends Component {
-//   render() {
-//     return <h1>Hello World ! </h1>;
-//   }
-// }
+import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
     <h1>Hello World !</h1>
+    <TodoList />
   </Provider>
 );
 
