536241d3c45e302d3f16e43c69a4ffced8c65e7e
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-07 15:49:12 -0200

    Nova Aplicação - Rotas

--- app/src/App.js
+++ app/src/App.js
@@ -4,15 +4,14 @@ import { Provider } from 'react-redux';
 import './store/config/reactotron';
 import store from './store';
 
-import TodoList from './TodoList';
+import Routes from './routes';
 
-console.tron.log('Hello World');
-console.tron.warn('Erro fatal');
+// console.tron.log('Hello World');
+// console.tron.warn('Erro fatal');
 
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World !</h1>
-    <TodoList />
+    <Routes />
   </Provider>
 );
 
