e1f29718e1fd64ae3257b100fd9b11222e011626
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-07 15:34:24 -0200

    Reactotron

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import './store/config/reactotron';
 import store from './store';
 
 import TodoList from './TodoList';
+
+console.tron.log('Hello World');
+console.tron.warn('Erro fatal');
 
 const App = () => (
   <Provider store={store}>
