cd5c067588b50f3befb1f79af4e7e6de62e78420
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-07 13:33:27 -0200

    init commit

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// class App extends Component {
+//   render() {
+//     return <h1>Hello World ! </h1>;
+//   }
+// }
+
+const App = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default App;
