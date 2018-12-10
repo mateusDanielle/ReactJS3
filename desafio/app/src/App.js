36d2364cb98e855bb60310f74daa0ee3423c9409
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-10 16:33:29 -0200

    Desafio - Mapa configurado

--- desafio/app/src/App.js
+++ desafio/app/src/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import ReactMapGl from 'react-map-gl';
 
 class App extends Component {
+  state = {
+    viewport: {
+      height: 600,
+      latitude: -23.56278,
+      longitude: -46.654383,
+      zoom: 15,
+    },
+  };
+
   render() {
-    return <h1>Olá</h1>;
+    const { viewport } = this.state;
+    return (
+      <ReactMapGl
+        {...viewport}
+        width="100%"
+        // height="50%"
+        onViewportChange={view => this.setState({ viewport: view })}
+        mapboxApiAccessToken="pk.eyJ1IjoibWF0ZXVzZGFuaWVsbGUiLCJhIjoiY2pwaW0xemhuMThuMjNrbWxkYTlqZ244aiJ9.QyzENcI2uUdt9PYiMTKOgg"
+      />
+    );
   }
 }
 
